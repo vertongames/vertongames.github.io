a2c8ff9633cdb7e9e1b5af232b4e757b4213188f
vertongames <alicornball@gmail.com>
2025-06-02 19:46:42 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,10 +1,10 @@
 function atb(content){ // add to body (atb)
-  document.body += content;
+  document.body.innerHTML += content;
   
 }
 
 function sb(content){ // set body (sb), reccomend to not use
-  document.body = content;
+  document.body.innerHTML = content;
 }
 
 atb("hello world");
