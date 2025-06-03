9c2a228fe1fa2efcb1f828067642431ff24213e3
vertongames <alicornball@gmail.com>
2025-06-02 19:58:58 -0700

    Update global.js

--- global.js
+++ global.js
@@ -7,4 +7,14 @@ function sb(content){ // set body (sb), reccomend to not use
   document.body.innerHTML = content;
 }
 
+function sh(content){ // set head (sh), reccomend to not use
+  document.head.innerHTML = content;
+}
+
+function ath(content){ // add to head (ath)
+  document.head.innerHTML += content;
+}
+
+document.title = "skibdii toilet";
+
 atb("hello world");
